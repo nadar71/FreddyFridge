28858d3a487b814905d3de709c1f35bc15e42635
simone <simone.mapelli@gmail.com>
2018-12-11 16:42:39 +0100

    Fab not working, work on it in the future

--- app/src/main/java/eu/indiewalkabout/fridgemanager/ui/MainActivity.java
+++ app/src/main/java/eu/indiewalkabout/fridgemanager/ui/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 import com.google.android.gms.ads.AdRequest;
@@ -19,12 +20,23 @@ import com.hitomi.cmlibrary.CircleMenu;
 import com.hitomi.cmlibrary.CircleMenuRectMain;
 import com.hitomi.cmlibrary.OnMenuSelectedListener;
 import com.hitomi.cmlibrary.OnMenuStatusChangeListener;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionButton;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionHelper;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionLayout;
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RFACLabelItem;
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RapidFloatingActionContentLabelList;
+
+import com.wangjie.rapidfloatingactionbutton.util.RFABShape;
+import com.wangjie.rapidfloatingactionbutton.util.RFABTextUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import eu.indiewalkabout.fridgemanager.R;
 import eu.indiewalkabout.fridgemanager.data.FoodDatabase;
 
-
 public class MainActivity extends AppCompatActivity {
+        // implements  RapidFloatingActionContentLabelList.OnRapidFloatingActionContentLabelListListener {
 
     // admob banner ref
     private AdView mAdView;
@@ -33,6 +45,11 @@ public class MainActivity extends AppCompatActivity {
     private CircleMenu circleMenu;
     private CircleMenuRectMain circleMenuRect;
     private Button temporarySettingsBtn;
+
+    // Rfab
+    private RapidFloatingActionLayout rfaLayout;
+    private RapidFloatingActionButton rfaBtn;
+    private RapidFloatingActionHelper rfabHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,9 +86,8 @@ public class MainActivity extends AppCompatActivity {
         mAdView.loadAd(adRequest);
 
 
-
         // -----------------------------------------------------------------------------------------
-        // Costum UI
+        // Circle Menu
         // -----------------------------------------------------------------------------------------
         // Log.d("MainActivity", "Before getting circleMenu from R");
 
@@ -81,40 +97,40 @@ public class MainActivity extends AppCompatActivity {
 
         //circleMenu.setMainMenu(Color.parseColor("#CDCDCD"), R.mipmap.icon_menu, R.mipmap.icon_cancel)
         circleMenu.setMainMenu(Color.parseColor("#740001"), R.drawable.ic_main_menu_icon, R.mipmap.icon_cancel)
-                  .addSubMenu(Color.parseColor("#258CFF"), R.mipmap.icon_home)
-                  .addSubMenu(Color.parseColor("#30A400"), R.mipmap.icon_search)
-                  .addSubMenu(Color.parseColor("#FF4B32"), R.mipmap.icon_notify)
-                  .addSubMenu(Color.parseColor("#FF4B32"), R.mipmap.icon_notify)
-                  //.addSubMenu(Color.parseColor("#8A39FF"), R.mipmap.icon_setting)
-                  //.addSubMenu(Color.parseColor("#FF6A00"), R.mipmap.icon_gps)
-                  .setOnMenuSelectedListener(new OnMenuSelectedListener() {
+                .addSubMenu(Color.parseColor("#258CFF"), R.mipmap.icon_home)
+                .addSubMenu(Color.parseColor("#30A400"), R.mipmap.icon_search)
+                .addSubMenu(Color.parseColor("#FF4B32"), R.mipmap.icon_notify)
+                .addSubMenu(Color.parseColor("#FF4B32"), R.mipmap.icon_notify)
+                //.addSubMenu(Color.parseColor("#8A39FF"), R.mipmap.icon_setting)
+                //.addSubMenu(Color.parseColor("#FF6A00"), R.mipmap.icon_gps)
+                .setOnMenuSelectedListener(new OnMenuSelectedListener() {
 
 
                     @Override
                     public void onMenuSelected(int index) { // btn numbered in clockwise direction from top
-                        switch(index){
-                            case 0 :
+                        switch (index) {
+                            case 0:
                                 Log.i("Insert New Food ", String.valueOf(index));
                                 Intent toInsertFood = new Intent(MainActivity.this, InsertFoodActivity.class);
                                 startActivity(toInsertFood);
                                 break;
 
 
-                            case 1 :
+                            case 1:
                                 Log.i("Show Expiring Food ", String.valueOf(index));
                                 Intent showExpiringFood = new Intent(MainActivity.this, FoodListActivity.class);
                                 showExpiringFood.putExtra(FoodListActivity.FOOD_TYPE, FoodListActivity.FOOD_EXPIRING);
                                 startActivity(showExpiringFood);
                                 break;
 
-                            case 2 :
+                            case 2:
                                 Log.i("Show SAVED Food", String.valueOf(index));
                                 Intent showSavedFood = new Intent(MainActivity.this, FoodListActivity.class);
                                 showSavedFood.putExtra(FoodListActivity.FOOD_TYPE, FoodListActivity.FOOD_SAVED);
                                 startActivity(showSavedFood);
                                 break;
 
-                            case 3 :
+                            case 3:
                                 Log.i("Show DEAD Food", String.valueOf(index));
                                 Intent showDeadFood = new Intent(MainActivity.this, FoodListActivity.class);
                                 showDeadFood.putExtra(FoodListActivity.FOOD_TYPE, FoodListActivity.FOOD_DEAD);
@@ -130,42 +146,89 @@ public class MainActivity extends AppCompatActivity {
                 }).setOnMenuStatusChangeListener(new OnMenuStatusChangeListener() {
 
             @Override
-            public void onMenuOpened() {}
+            public void onMenuOpened() {
+            }
 
             @Override
-            public void onMenuClosed() {}
+            public void onMenuClosed() {
+            }
 
         });
 
+        // -----------------------------------------------------------------------------------------
+        // TODO : Rfab instance
+        // -----------------------------------------------------------------------------------------
         /*
-        // variante con menu rettangolare
-        circleMenuRect = (CircleMenuRectMain) findViewById(R.id.circle_menu_rect); //
+        RapidFloatingActionContentLabelList rfaContent = new RapidFloatingActionContentLabelList(this);
 
-        // Log.d("MainActivity", "Before circleMenu.setMainMenu");
+        rfaContent.setOnRapidFloatingActionContentLabelListListener(this);
 
-        //circleMenu.setMainMenu(Color.parseColor("#CDCDCD"), R.mipmap.icon_menu, R.mipmap.icon_cancel)
-        circleMenuRect.setMainMenu(Color.parseColor("#740001"), R.drawable.ic_main_menu_icon, R.mipmap.icon_cancel)
-                .addSubMenu(Color.parseColor("#258CFF"), R.mipmap.icon_home)
-                .addSubMenu(Color.parseColor("#30A400"), R.mipmap.icon_search)
-                .addSubMenu(Color.parseColor("#FF4B32"), R.mipmap.icon_notify)
-                //.addSubMenu(Color.parseColor("#8A39FF"), R.mipmap.icon_setting)
-                //.addSubMenu(Color.parseColor("#FF6A00"), R.mipmap.icon_gps)
-                .setOnMenuSelectedListener(new OnMenuSelectedListener() {
+        List<RFACLabelItem> items = new ArrayList<>();
 
-                    @Override
-                    public void onMenuSelected(int index) {}
+        items.add(new RFACLabelItem<Integer>()
+                .setLabel("icon d")
+                .setResId(R.mipmap.icon_home)
+                .setIconNormalColor(0xffd84315)
+                .setIconPressedColor(0xffbf360c)
+                .setWrapper(0)
+        );
+        items.add(new RFACLabelItem<Integer>()
+                .setLabel("icon c")
+                .setResId(R.mipmap.icon_gps)
+                .setIconNormalColor(0xff4e342e)
+                .setIconPressedColor(0xff3e2723)
+                .setLabelColor(Color.WHITE)
+                .setLabelSizeSp(14)
+                .setLabelBackgroundDrawable(RFABShape.generateCornerShapeDrawable(0xaa000000, RFABTextUtil.dip2px(this, 4)))
+                .setWrapper(1)
+        );
+        items.add(new RFACLabelItem<Integer>()
+                .setLabel("icon b")
+                .setResId(R.mipmap.icon_cancel)
+                .setIconNormalColor(0xff056f00)
+                .setIconPressedColor(0xff0d5302)
+                .setLabelColor(0xff056f00)
+                .setWrapper(2)
+        );
 
-                }).setOnMenuStatusChangeListener(new OnMenuStatusChangeListener() {
+        items.add(new RFACLabelItem<Integer>()
+                .setLabel("icon a")
+                .setResId(R.mipmap.icon_menu)
+                .setIconNormalColor(0xff283593)
+                .setIconPressedColor(0xff1a237e)
+                .setLabelColor(0xff283593)
+                .setWrapper(3)
+        );
 
-            @Override
-            public void onMenuOpened() {}
+        rfaContent
+                .setItems(items)
+                .setIconShadowRadius(RFABTextUtil.dip2px(this, 5))
+                .setIconShadowColor(0xff888888)
+        .setIconShadowDy(RFABTextUtil.dip2px(this, 5))
+        ;
 
-            @Override
-            public void onMenuClosed() {}
-
-        });
-        */
+        rfabHelper = new RapidFloatingActionHelper(
+                this,
+                rfaLayout,
+                rfaBtn,
+                rfaContent
+        ).build();
     }
+
+    @Override
+    public void onRFACItemLabelClick(int position, RFACLabelItem item) {
+        Toast.makeText(this, "clicked label: " + position, Toast.LENGTH_SHORT).show();
+        rfabHelper.toggleContent();
+    }
+
+    @Override
+    public void onRFACItemIconClick(int position, RFACLabelItem item) {
+        Toast.makeText(this, "clicked icon: " + position, Toast.LENGTH_SHORT).show();
+        rfabHelper.toggleContent();
+    }
+
+*/
+
 
     // ---------------------------------------------------------------------------------------------
     //                                          MENU STUFF
