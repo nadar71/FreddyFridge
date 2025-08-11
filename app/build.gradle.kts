692fd2e0884db053de48743eb4afdace2af973ac
simone <simone.mapelli@gmail.com>
2025-08-11 14:25:52 +0200

    chore: set 2.0.1_11 and release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "eu.indiewalkabout.fridgemanager"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10 
-        versionName = "2.0.0"
+        versionCode = 11
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
