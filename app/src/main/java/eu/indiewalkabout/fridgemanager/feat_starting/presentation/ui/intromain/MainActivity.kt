595db6518e41f36290cf27b85d93b45601489617
simone <simone.mapelli@gmail.com>
2025-09-12 13:04:19 +0200

    fix: reverted last commit for better solution

--- app/src/main/java/eu/indiewalkabout/fridgemanager/feat_starting/presentation/ui/intromain/MainActivity.kt
+++ app/src/main/java/eu/indiewalkabout/fridgemanager/feat_starting/presentation/ui/intromain/MainActivity.kt
@@ -33,6 +33,7 @@ import eu.indiewalkabout.fridgemanager.feat_notifications.util.extensions.needsE
 import eu.indiewalkabout.fridgemanager.feat_notifications.util.extensions.openAlarmSettings
 
 
+@AndroidEntryPoint
 class MainActivity: AppCompatActivity()  {
     val TAG = "MainActivity"
 
